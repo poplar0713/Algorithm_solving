9fe2741e35d0411976fa4040bc6e89ad5ab76922
여정동 <duwjdehd0713@gmail.com>
2022-02-16 10:43:52 +0900

    프로그래머스 행렬 테두리 회전하기

--- solved/Programmers/행렬테두리회전하기.java
+++ solved/Programmers/행렬테두리회전하기.java
@@ -2,7 +2,6 @@
 //행렬테두리회전하기
 public class Solution {
 	static int[][] map;
-    
     static void generateMap(int rows, int columns){
         map = new int[rows+1][columns+1];
         int num = 1;
@@ -13,7 +12,7 @@ public class Solution {
         }
     }
     
-    static int rotate(int[] q){  //행1 = [0], 열1 = [1], 행2 = [2], 열2 = [3]
+    static int rotate(int[] q){
         int min = Integer.MAX_VALUE;
         int x0fin = map[q[0]][q[3]];
         int x1fin = map[q[2]][q[1]];
@@ -48,23 +47,19 @@ public class Solution {
         generateMap(rows, columns);
         for(int i = 0; i < queries.length; i++){
             answer[i] = rotate(queries[i]);
-            
-            for(int j = 1; j <= rows; j++) {
-            	for(int n = 1; n <= columns; n++) {
-            		System.out.print(map[j][n] + " ");
-            	}
-            	System.out.println();
-            }
-            System.out.println();
+//            for(int j = 1; j <= rows; j++) {
+//            	for(int n = 1; n <= columns; n++) {
+//            		System.out.print(map[j][n] + " ");
+//            	}
+//            	System.out.println();
+//            }
+//            System.out.println();
         }
-        
         return answer;
     }
 	
-	public static void main(String[] args) {
-		 
+	public static void main(String[] args) {		 
 		int[][] queries = {{1, 1, 2, 2}, {1, 2, 2, 3}, {2, 1, 3, 2}, {2, 2, 3, 3}};
-		
 		Solution sol = new Solution();
 		for(int n : sol.solution(3, 3, queries)) {
 			System.out.print(n);
